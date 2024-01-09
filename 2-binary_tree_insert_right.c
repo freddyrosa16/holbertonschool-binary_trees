a0613042affadd65c1784a617bce1df6e978db24
MorrisCastro <jomarcastro2004@gmail.com>
2024-01-09 10:47:51 -0400

    fix the tabs

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,20 +7,20 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *temp;
+	binary_tree_t *temp;
 
-    if (parent == NULL)
-        return (NULL);
-    
-    temp = binary_tree_node(parent, value);
-    if (temp == NULL)
-        return (NULL);
-    
-    if (parent->right != NULL)
-    {
-        temp->right = parent->right;
-        parent->right->parent = temp;
-    }
-    parent->right = temp;
-    return (temp);
+	if (parent == NULL)
+		return (NULL);
+
+	temp = binary_tree_node(parent, value);
+	if (temp == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		temp->right = parent->right;
+		parent->right->parent = temp;
+	}
+	parent->right = temp;
+	return (temp);
 }
