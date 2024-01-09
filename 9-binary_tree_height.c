ef782428da8c9c6d3a589333e4eca76ec9abbe67
freddyrosa16 <freddy.rosa16@gmail.com>
2024-01-09 18:31:33 -0400

    Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,7 +6,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t leftHeight = 0, rightHeight = 0;
+	size_t leftHeight = 0, rightHeight = 0;
 
 	if (tree == NULL)
 		return (0);
