0163b9c9a7e406e8f1b878e36c2dcb30dbe6ff15
MorrisCastro <jomarcastro2004@gmail.com>
2024-01-10 10:00:46 -0400

    add funcion to run trough the inside of the tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,8 +1,31 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_balance_rec - run through the tree
+ * @tree: pointer to the root
+ * Return: return the greater side
+*/
+int binary_tree_balance_rec(const binary_tree_t *tree)
+{
+	int leftHeight = 0, rightHeight = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		leftHeight = binary_tree_balance_rec(tree->left) + 1;
+
+	if (tree->right != NULL)
+		rightHeight = binary_tree_balance_rec(tree->right) + 1;
+
+	if (rightHeight > leftHeight)
+		return (rightHeight);
+	else
+		return (leftHeight);
+}
+/**
  * binary_tree_balance - measure the balance of the tree
  * @tree: pointer to the root
- * Return: return the balance of the tree
+ * Return: Return the balance of the tree
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
@@ -12,10 +35,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL)
-		leftHeight = binary_tree_balance(tree->left) + 1;
+		leftHeight = binary_tree_balance_rec(tree->left) + 1;
 
 	if (tree->right != NULL)
-		rightHeight = binary_tree_balance(tree->right) + 1;
+		rightHeight = binary_tree_balance_rec(tree->right) + 1;
 
 	return (leftHeight - rightHeight);
 }
